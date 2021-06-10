e6aaf956625bb36f82f22181c903b5485773bbc6
Freddiiy <frederikgaller@live.dk>
2021-06-10 17:08:35 +0200

    update

--- app/render.js
+++ app/render.js
@@ -1,18 +1,36 @@
+const connect = document.getElementById('ipinput');
+
 const nameInput = document.getElementById('nameInput');
 const textinput = document.getElementById('textInput');
 const sendButton = document.getElementById('sendButton');
 const chatarea = document.getElementById('chatarea');
 
+//const connect = '83.94.37.212:6969';
 
-const connect = 'localhost:6969';
-
-const ws = new WebSocket('ws://' + connect);
-
+//const ws = new WebSocket('ws://' + connect);
+var ws = new WebSocket('ws://localhost:6965');
 
 console.log("Started");
 
 textinput.addEventListener('keydown', sendEnter);
 sendButton.addEventListener('click', sendMessage);
+
+
+connect.addEventListener('keydown', connectToWS);
+function connectToWS() {
+    if (event.keyCode === 13) {
+        ws = new WebSocket('ws://' + connect.value);
+        console.log("Connecting to " + connect.value + "...");
+        ws.onopen = () => {
+            console.log("Connection established...");
+            let tmp = `<div class="connected"> <p class="current-msg">You are connected.</p> </div>`;
+            chatarea.insertAdjacentHTML("beforeend", tmp);
+        }
+        recieveMessage();
+    }
+}
+
+
 //send message
 function sendMessage() {
     let username = nameInput.value;
@@ -24,9 +42,10 @@ function sendMessage() {
         }
         let tmp = `<div class="sent-msg"> <p class="current-msg">${userInput}</p> </div>`;
         chatarea.insertAdjacentHTML("beforeend", tmp);
-
+        
         let dataArray = [username, userInput];
-        ws.send(dataArray);
+        let jsonData= JSON.stringify(dataArray);
+        ws.send(jsonData);
     }
     textinput.value = "";
 }
@@ -38,11 +57,22 @@ function sendEnter() {
 }
 
 //recieve
-ws.onmessage = ({ data }) => {
-    let dataArray = data.split(",");
-    let username = dataArray[0];
-    let userInput = dataArray[1];
-    console.log(data);
-    let tmp = `<span class="username">${username}</span><div class="recieved-msg"><p class="current-msg">${userInput}</p> </div>`;
-    chatarea.insertAdjacentHTML("beforeend", tmp);
-};
+function recieveMessage() {
+    ws.onmessage = ({ data }) => {
+        let dataArray = JSON.parse(data);
+        let username = dataArray[0];
+        let userInput = dataArray[1];
+        let tmp = `<span class="username">${username}</span><div class="recieved-msg"><p class="current-msg">${userInput}</p> </div>`;
+        chatarea.insertAdjacentHTML("beforeend", tmp);
+    };
+}
+
+ws.onopen = () => {
+    console.log("WEBSOCKET OPEN");
+}
+ws.onclose = () => {
+    console.log("WEBSOCKET OPEN");
+}
+ws.onerror = () => {
+    console.log("WEBSOCKET ERROR");
+}
--- server/server.js
+++ server/server.js
@@ -5,13 +5,16 @@ const port = 6969;
 const wss = new WebSocket.Server({ port: port });
 
 wss.on('connection', function connection(ws) {
-  ws.on('', function incoming(dataArray) {
+  ws.on('message', function incoming(dataArray) {
     wss.clients.forEach(function each(client) {
+      console.log("client is not itself: ", client !== ws);
+      console.log("client is open: ", client.readyState === WebSocket.OPEN);
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(dataArray);
+        console.log("data sent back: ", dataArray);
       }
     })
-    console.log('received: %s', message);
+    console.log('received: %s', dataArray);
   });
 });
 console.log("listening on port: " + port);
