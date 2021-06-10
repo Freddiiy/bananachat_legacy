9f1f75c10e299f6ccc27c30769a6200747b247f3
Freddiiy <frederikgaller@live.dk>
2021-06-10 22:03:46 +0200

    update

--- server/server.js
+++ server/server.js
@@ -1,20 +1,35 @@
 const WebSocket = require('ws');
+const express = require('express');
+const app = express();
 
-const port = 6969;
+const appPort = process.env.PORT || 80;
+const port = process.env.PORT ||6969;
 
+//serves the webpage
+app.use(express.static("public"));
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+  console.log("serving webpage");
+});
+
+app.listen(appPort);
+console.log("Website live on: " + appPort);
+
+//backend websocket
 const wss = new WebSocket.Server({ port: port });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(dataArray) {
     wss.clients.forEach(function each(client) {
-      console.log("client is not itself: ", client !== ws);
-      console.log("client is open: ", client.readyState === WebSocket.OPEN);
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(dataArray);
         console.log("data sent back: ", dataArray);
       }
     })
-    console.log('received: %s', dataArray);
+    let data = JSON.parse(dataArray);
+    let username = data[0];
+    let userInput = data[1];
+    console.log(username + ' says: ' + userInput);
   });
 });
 console.log("listening on port: " + port);
