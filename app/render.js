ad888666e8ec9f78d54721f159ae06399cf5a06e
Freddiiy <frederikgaller@live.dk>
2021-06-14 14:27:49 +0200

    added img support

--- app/render.js
+++ app/render.js
@@ -4,19 +4,28 @@ const checkbox = document.getElementById('checkbox');
 const nameInput = document.getElementById('nameInput');
 const textinput = document.getElementById('textInput');
 const fileSelector = document.getElementById('file-selector');
+const imgPreview = document.getElementById('img-preview-window');
+
+const startCall = document.getElementById('startCall');
+
 const sendButton = document.getElementById('sendButton');
 const chatarea = document.getElementById('chatarea');
 
-var HOST = 'wss://localhost.com:6969';
+const url = window.location.href;
+var HOST = url.replace(/(http)(s)?\:\/\//, "ws$2://");
+console.log();
 var firstRun = true;
-var ws = new WebSocket(HOST);
+var ws = new WebSocket('ws://localhost:80');
 var imageNum = 0;
+var connected = false;
 
 console.log("Started");
 
-fileSelector.addEventListener('change', sendImage);
+fileSelector.addEventListener('change', imagePreview);
 textinput.addEventListener('keydown', sendEnter);
 sendButton.addEventListener('click', sendMessage);
+
+startCall.addEventListener('click', voicecall);
 
 connect.addEventListener('keydown', connectToWS);
 function connectToWS() {
@@ -26,12 +35,6 @@ function connectToWS() {
             ws.close();
         }
         firstRun = false;
-
-        ws.onclose = () => {
-            console.log("Connection disconnected...");
-            let tmp = `<div class="connected"> <p class="current-msg">You have been disconnected.</p> </div>`;
-            chatarea.insertAdjacentHTML("beforeend", tmp);
-        }
         if (connect.value !== "") {
             if (checkbox.checked) {
                 ws = new WebSocket('wss://' + connect.value);
@@ -40,11 +43,6 @@ function connectToWS() {
             }
         }
         console.log("Connecting to " + connect.value + "...");
-        ws.onopen = () => {
-            console.log("Connection established...");
-            let tmp = `<div class="connected"> <p class="current-msg">You are connected.</p> </div>`;
-            chatarea.insertAdjacentHTML("beforeend", tmp);
-        }
         recieveMessage();
     }
 }
@@ -57,30 +55,54 @@ function sendMessage() {
         if(username == "") {
             username = "anon";
         }
-        let tmp = `<div class="sent-msg"> <p class="current-msg">${userInput}</p> </div>`;
-        chatarea.insertAdjacentHTML("beforeend", tmp);
+        if(userInput !== "") {
+            let tmp = `<div class="sent-msg"> <p class="current-msg">${userInput}</p> </div>`;
+            chatarea.insertAdjacentHTML("beforeend", tmp);
         
-        let dataArray = ["message", username, userInput];
-        let jsonData= JSON.stringify(dataArray);
-        ws.send(jsonData);
-
-        if (fileSelector.value !== "") {
-            sendImage();
-            console.log("send image");
+            let dataArray = ["message", username, userInput];
+            let jsonData= JSON.stringify(dataArray);
+            ws.send(jsonData);
         }
     }
 
+    if (fileSelector.value !== "") {
+        console.log("send image");
+        sendImage();
+    }
+
     textinput.value = "";
-    fileSelector.value = "";
+    fileSelector.value = null;
+}
+
+function imagePreview() {
+    while(imgPreview.firstChild) {
+        imgPreview.removeChild(imgPreview.firstChild);
+    }
+
+    let imgFile = document.getElementById("file-selector").files[0];
+    let fileReader = new FileReader();
+    fileReader.onload = function (e){
+        let img = document.createElement("img");
+        img.id = "img-preview";
+        img.src = e.target.result;
+        imgPreview.appendChild(img)
+    }
+    fileReader.readAsDataURL(imgFile);
+
+    textinput.focus();
 }
 
 function sendImage() {
+    while(imgPreview.firstChild) {
+        imgPreview.removeChild(imgPreview.firstChild);
+    }
+
     let username = nameInput.value;
     if(username == "") {
         username = "anon";
     }
 
-    let tmp = `<div class="sent-img"><div id="imageNum-${imageNum}"></div></div>`;
+    let tmp = `<div class="sent-img"><div class="current-img" id="imageNum-${imageNum}"></div></div>`;
     chatarea.insertAdjacentHTML("beforeend", tmp);
     var imgElement = document.getElementById("imageNum-" + imageNum);
 
@@ -100,7 +122,6 @@ function sendImage() {
         ws.send(jsonData)
     }
     fileReader.readAsDataURL(imgFile);
-;
     imageNum++;
 }
 
@@ -110,6 +131,7 @@ function sendEnter() {
     }
 }
 
+recieveMessage();
 function recieveMessage() {
     ws.onmessage = ({ data }) => {
         let dataArray = JSON.parse(data);
@@ -123,7 +145,7 @@ function recieveMessage() {
             let username = dataArray[1];
             let rawData = dataArray[2];
 
-            let tmp = `<span class="username">${username}</span><div class="recieved-img"><div id="imageNum-${imageNum}"></div>`;
+            let tmp = `<span class="username">${username}</span><div class="recieved-img"><div class="current-img" id="imageNum-${imageNum}"></div>`;
             chatarea.insertAdjacentHTML("beforeend", tmp);
 
             let img = document.createElement("img");
@@ -133,4 +155,16 @@ function recieveMessage() {
         }
         imageNum++;
     };
+}
+
+ws.onopen = () => {
+    console.log("Connection established...");
+    let tmp = `<div class="connected"> <p class="current-msg">You are connected.</p> </div>`;
+    chatarea.insertAdjacentHTML("beforeend", tmp);
+}
+
+ws.onclose = () => {
+    console.log("Connection disconnected...");
+    let tmp = `<div class="connected"> <p class="current-msg">You have been disconnected.</p> </div>`;
+    chatarea.insertAdjacentHTML("beforeend", tmp);
 }
--- server/public/render.js
+++ server/public/render.js
@@ -3,18 +3,25 @@ const checkbox = document.getElementById('checkbox');
 
 const nameInput = document.getElementById('nameInput');
 const textinput = document.getElementById('textInput');
+const fileSelector = document.getElementById('file-selector');
+const imgPreview = document.getElementById('img-preview-window');
+
 const sendButton = document.getElementById('sendButton');
 const chatarea = document.getElementById('chatarea');
 
-var HOST = 'wss://localhost.com:6969';
+const url = window.location.href;
+var HOST = url.replace(/(http)(s)?\:\/\//, "ws$2://");
+console.log();
 var firstRun = true;
-var ws = new WebSocket(HOST);
+var ws = new WebSocket('ws://localhost:80');
+var imageNum = 0;
+var connected = false;
 
 console.log("Started");
 
+fileSelector.addEventListener('change', imagePreview);
 textinput.addEventListener('keydown', sendEnter);
 sendButton.addEventListener('click', sendMessage);
-
 
 connect.addEventListener('keydown', connectToWS);
 function connectToWS() {
@@ -24,12 +31,6 @@ function connectToWS() {
             ws.close();
         }
         firstRun = false;
-
-        ws.onclose = () => {
-            console.log("Connection disconnected...");
-            let tmp = `<div class="connected"> <p class="current-msg">You have been disconnected.</p> </div>`;
-            chatarea.insertAdjacentHTML("beforeend", tmp);
-        }
         if (connect.value !== "") {
             if (checkbox.checked) {
                 ws = new WebSocket('wss://' + connect.value);
@@ -37,19 +38,11 @@ function connectToWS() {
                 ws = new WebSocket('ws://' + connect.value);
             }
         }
-
         console.log("Connecting to " + connect.value + "...");
-        ws.onopen = () => {
-            console.log("Connection established...");
-            let tmp = `<div class="connected"> <p class="current-msg">You are connected.</p> </div>`;
-            chatarea.insertAdjacentHTML("beforeend", tmp);
-        }
         recieveMessage();
     }
 }
 
-
-//send message
 function sendMessage() {
     let username = nameInput.value;
     let userInput = textinput.value;
@@ -58,14 +51,74 @@ function sendMessage() {
         if(username == "") {
             username = "anon";
         }
-        let tmp = `<div class="sent-msg"> <p class="current-msg">${userInput}</p> </div>`;
-        chatarea.insertAdjacentHTML("beforeend", tmp);
+        if(userInput !== "") {
+            let tmp = `<div class="sent-msg"> <p class="current-msg">${userInput}</p> </div>`;
+            chatarea.insertAdjacentHTML("beforeend", tmp);
         
-        let dataArray = [username, userInput];
-        let jsonData= JSON.stringify(dataArray);
-        ws.send(jsonData);
+            let dataArray = ["message", username, userInput];
+            let jsonData= JSON.stringify(dataArray);
+            ws.send(jsonData);
+        }
     }
+
+    if (fileSelector.value !== "") {
+        console.log("send image");
+        sendImage();
+    }
+
     textinput.value = "";
+    fileSelector.value = null;
+}
+
+function imagePreview() {
+    while(imgPreview.firstChild) {
+        imgPreview.removeChild(imgPreview.firstChild);
+    }
+
+    let imgFile = document.getElementById("file-selector").files[0];
+    let fileReader = new FileReader();
+    fileReader.onload = function (e){
+        let img = document.createElement("img");
+        img.id = "img-preview";
+        img.src = e.target.result;
+        imgPreview.appendChild(img)
+    }
+    fileReader.readAsDataURL(imgFile);
+
+    textinput.focus();
+}
+
+function sendImage() {
+    while(imgPreview.firstChild) {
+        imgPreview.removeChild(imgPreview.firstChild);
+    }
+
+    let username = nameInput.value;
+    if(username == "") {
+        username = "anon";
+    }
+
+    let tmp = `<div class="sent-img"><div class="current-img" id="imageNum-${imageNum}"></div></div>`;
+    chatarea.insertAdjacentHTML("beforeend", tmp);
+    var imgElement = document.getElementById("imageNum-" + imageNum);
+
+    let imgFile = document.getElementById("file-selector").files[0];
+    let fileReader = new FileReader();
+    var rawFile = new ArrayBuffer();
+    fileReader.onload = function (e){
+        let img = document.createElement("img");
+
+        img.src = e.target.result;
+        imgElement.appendChild(img);
+
+        rawFile = e.target.result;
+
+        let dataArray = ["file", username, rawFile];
+        let jsonData = JSON.stringify(dataArray)
+        ws.send(jsonData)
+    }
+    fileReader.readAsDataURL(imgFile);
+    imageNum++;
 }
 
 function sendEnter() {
@@ -74,13 +127,40 @@ function sendEnter() {
     }
 }
 
-//recieve
+recieveMessage();
 function recieveMessage() {
     ws.onmessage = ({ data }) => {
         let dataArray = JSON.parse(data);
-        let username = dataArray[0];
-        let userInput = dataArray[1];
-        let tmp = `<span class="username">${username}</span><div class="recieved-msg"><p class="current-msg">${userInput}</p> </div>`;
-        chatarea.insertAdjacentHTML("beforeend", tmp);
+        if (dataArray[0] == "message") {
+            let username = dataArray[1];
+            let userInput = dataArray[2];
+            let tmp = `<span class="username">${username}</span><div class="recieved-msg"><p class="current-msg">${userInput}</p> </div>`;
+            chatarea.insertAdjacentHTML("beforeend", tmp);
+
+        } else if (dataArray[0] == "file") {
+            let username = dataArray[1];
+            let rawData = dataArray[2];
+
+            let tmp = `<span class="username">${username}</span><div class="recieved-img"><div class="current-img" id="imageNum-${imageNum}"></div>`;
+            chatarea.insertAdjacentHTML("beforeend", tmp);
+
+            let img = document.createElement("img");
+            img.src = rawData;
+            var imgElement = document.getElementById("imageNum-" + imageNum);
+            imgElement.appendChild(img);
+        }
+        imageNum++;
     };
+}
+
+ws.onopen = () => {
+    console.log("Connection established...");
+    let tmp = `<div class="connected"> <p class="current-msg">You are connected.</p> </div>`;
+    chatarea.insertAdjacentHTML("beforeend", tmp);
+}
+
+ws.onclose = () => {
+    console.log("Connection disconnected...");
+    let tmp = `<div class="connected"> <p class="current-msg">You have been disconnected.</p> </div>`;
+    chatarea.insertAdjacentHTML("beforeend", tmp);
 }
