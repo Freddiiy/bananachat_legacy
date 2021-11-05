4f568d4e6dd8cd3e1e2ee852e4518406948326b7
Freddiiy <frederikgaller@live.dk>
2021-11-05 15:32:41 +0100

    asd

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 6969;
 
 //serves the webpage
 app.use(express.static("public"));
-app.get('*', function(req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
   console.log("serving webpage");
 });
