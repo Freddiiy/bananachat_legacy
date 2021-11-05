f3336d80203d1cad76d41409a58a0bd9b049aef6
Freddiiy <frederikgaller@live.dk>
2021-11-05 15:57:59 +0100

    asd

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ const express = require('express');
 const app = express();
 const WebSocket = require('ws');
 
-const Datastore = require('nedb');
-const db = new Datastore('messages.db');
-db.loadDatabase();
-
 const port = process.env.PORT || 6969;
 
 //serves the webpage
@@ -19,15 +15,8 @@ app.get('*', function (req, res) {
 const wss = new WebSocket.Server({ noServer: true });
 
 wss.on('connection', function connection(ws) {
-  let allMsg = db.getAllData();
-  for (let i = 0; i < allMsg.length; i++) {
-    let dbData = JSON.stringify(allMsg[i]);
-    ws.send(dbData);
-  }
-
   ws.on('message', function incoming(dataArray) {
     let data = JSON.parse(dataArray);
-    db.insert(data);
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(dataArray);
